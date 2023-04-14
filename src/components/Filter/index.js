73f97c396d4ed331922c1c15a2534b92db4bf7b1
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-04-14 16:18:58 +0330

    add some style

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -16,25 +16,27 @@ const Filter = () => {
   }
 
   return (
-    <section className="mb-8 md:mb-9 flex flex-col gap-y-8 lg:flex-row lg:justify-between lg:gap-x-11 lg:gap-y-0 relative px-6 lg:px-16">
-      <FilterForm
-        title="Pick-Up"
-        bgColor="bg-primary-500"
-        date={pickUptDate}
-        handleDateChange={handlePickupDateChange}
-      />
-      <Link
-        href="/cars"
-        className="self-center bg-primary-500 w-[60px] h-[60px] hover:bg-primary-600 rounded-[10px] flex items-center justify-center absolute top-[125px] lg:static "
-      >
-        <DoubleArrow />
-      </Link>
-      <FilterForm
-        title="Drop-Off"
-        bgColor="bg-information-500"
-        date={dropOffDate}
-        handleDateChange={handleDropoffDateChange}
-      />
+    <section className="px-6 lg:px-16 ">
+      <div className="mb-8 md:mb-9 flex flex-col gap-y-8 lg:flex-row lg:justify-between lg:gap-x-11 lg:gap-y-0 relative container mx-auto max-w-[1440px]">
+        <FilterForm
+          title="Pick-Up"
+          bgColor="bg-primary-500"
+          date={pickUptDate}
+          handleDateChange={handlePickupDateChange}
+        />
+        <Link
+          href="/cars"
+          className="self-center bg-primary-500 w-[60px] h-[60px] hover:bg-primary-600 rounded-[10px] flex items-center justify-center absolute top-[125px] lg:static "
+        >
+          <DoubleArrow />
+        </Link>
+        <FilterForm
+          title="Drop-Off"
+          bgColor="bg-information-500"
+          date={dropOffDate}
+          handleDateChange={handleDropoffDateChange}
+        />
+      </div>
     </section>
   );
 };
